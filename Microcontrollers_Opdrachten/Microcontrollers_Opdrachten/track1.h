beadf45001d9e5988b0ece05fe2262aa4d30789b
Dion <dionvanderlinden@live.nl>
2019-02-06 10:47:35 +0100

    Track 1 in aparte header en source files

---
+++ Microcontrollers_Opdrachten/Microcontrollers_Opdrachten/track1.h
@@ -0,0 +1,20 @@
+/*
+ * track1.h
+ *
+ * Created: 6-2-2019 10:38:39
+ *  Author: Dion van der Linden
+ */ 
+
+
+#ifndef TRACK1_H_
+#define TRACK1_H_
+
+void b2();
+void b3();
+void b4();
+void b5();
+void b6();
+void b7b();
+void setCharliePlexingLed(int lednr);
+
+#endif /* TRACK1_H_ */
