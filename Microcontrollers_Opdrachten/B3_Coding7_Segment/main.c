7a1d8592fa6091aa0eee6e8cfceeeb887c18a44c
Dion <dionvanderlinden@live.nl>
2019-02-06 16:11:36 +0100

    Code werkend gemaakt smh

--- Microcontrollers_Opdrachten/B3_Coding7_Segment/main.c
+++ Microcontrollers_Opdrachten/B3_Coding7_Segment/main.c
@@ -4,7 +4,6 @@
  * Created: 6-2-2019 14:51:05
  * Author : Dion van der Linden
  */ 
-#define PINDA PIND
 
 #include "utils.h"
 #include <avr/io.h>
@@ -14,61 +13,58 @@
 
 void display(int);
 
+
 int main(void)
 {
 	// Init I/O
 	DDRD = 0xFF;			// output
 	DDRC = 0x00;			// input,   PORTD(7:4) output, PORTD(3:0) input	1111 0000
 		
-	
-	
-	
 	int num = 0;				
 	
     while (1) 
     {
-		if(1 & PINC)
+		if ((0x01 & PINC) && (0x02 & PINC))
+		{
+			num = 0;
+		}
+		else if(0x01 & PINC)
 		{
 			num++;	
-		}
-		
-		if(2 & PINC)
+		}		
+		else if(0x02 & PINC)
 		{
 			num--;	
 		}
 		
-		if(3 & PINC)
-		{
-			num = 0;
-		}
 		display(num);
 		wait(100);
 	}
 	
-	
+	return 0;
 }
+
+const int numbers[20] = {
+	0b00111111, // 0
+	0b00000110, // 1
+	0b01011011, // 2
+	0b01001111, // 3
+	0b01100110, // 4
+	0b01101101, // 5
+	0b01111101, // 6
+	0b00000111, // 7
+	0b01111111, // 8
+	0b01101111, // 9
+	0b01110111, // A
+	0b01111100, // B
+	0b00111001, // C
+	0b01011110, // D
+	0b01111001, // E
+	0b01110001, // F
+};
 
 void display(int num)
 {
-	int numbers[20] = {
-		0b00111111, // 0
-		0b00000110, // 1 
-		0b01011011, // 2 
-		0b01001111, // 3 
-		0b01100110, // 4 
-		0b01101101, // 5 
-		0b01111101, // 6 
-		0b00000111, // 7 
-		0b01111111, // 8 
-		0b01101111, // 9 
-		0b01110111, // A 
-		0b01111100, // B 
-		0b00111001, // C 
-		0b01011110, // D 
-		0b01111001, // E       
-		0b01110001, // F	
-	};
-	
 	if( num < 16 && num >=0 )
 		PORTD = numbers[num];
 	else
