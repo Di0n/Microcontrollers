347a6def7fb2b3a7b52bf7aa22a677a58bb31235
Yannick van Dolen <ydvd1308@gmail.com>
2019-02-06 15:57:52 +0100

    Updates niet werkende t2d3

---
+++ Microcontrollers_Opdrachten/B3_Coding7_Segment/utils.h
@@ -0,0 +1,18 @@
+/*
+ * utils.h
+ *
+ *	Created: 30-1-2019 11:13:49
+ *  Author: Dion van der Linden
+ *	Description: Verschillende utilities voor programma
+ */ 
+
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#define F_CPU 8000000L
+
+void wait(int ms);
+
+
+#endif /* UTILS_H_ */
