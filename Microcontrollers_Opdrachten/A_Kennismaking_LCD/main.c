722e3a6332f4425f1477eef8a5b7cb4f6e864539
Yannick van Dolen <ydvd1308@gmail.com>
2019-02-13 11:38:06 +0100

    Basis track 3

---
+++ Microcontrollers_Opdrachten/A_Kennismaking_LCD/main.c
@@ -0,0 +1,18 @@
+/*
+ * A_Kennismaking_LCD.c
+ *
+ * Created: 13-2-2019 11:37:08
+ * Author : Yannick
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
