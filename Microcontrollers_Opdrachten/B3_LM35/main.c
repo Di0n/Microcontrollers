ca38652d618f4fbf033a1e2b3a9e5911c9ea4a02
Yannick van Dolen <ydvd1308@gmail.com>
2019-02-20 12:53:17 +0100

    Init wk4b3

---
+++ Microcontrollers_Opdrachten/B3_LM35/main.c
@@ -0,0 +1,18 @@
+/*
+ * B3_LM35.c
+ *
+ * Created: 20-2-2019 12:49:56
+ * Author : Yannick
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
